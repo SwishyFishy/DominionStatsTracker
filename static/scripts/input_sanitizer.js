8d98fa1198e89a8f180241bb3f194a0b1c089c9a
Jonah Galloway-Fenwick <jonahgallowayfenwick@protonmail.com>
2025-12-12 12:42:47 -0500

    Add semi-colons for consistency

--- static/scripts/input_sanitizer.js
+++ static/scripts/input_sanitizer.js
@@ -1,5 +1,5 @@
 // Add event handler to player 3 name dropdown which requires the otehr player 3 field be filled out if a player 3 is selected
-p3Selector = document.getElementById("p3")
+p3Selector = document.getElementById("p3");
 p3Selector.addEventListener("change", () => {
     [...document.getElementsByClassName("p3_input")].forEach((input) => {
         if (p3Selector.value != '')
@@ -10,11 +10,11 @@ p3Selector.addEventListener("change", () => {
         {
             input.removeAttribute("required");
         }
-    })
-})
+    });
+});
 
 // Add event handler to player 4 name dropdown which requires the other player 4 fields and the player 3 fields to be filled out if a player 4 is selected
-p4Selector = document.getElementById("p4")
+p4Selector = document.getElementById("p4");
 p4Selector.addEventListener("change", () => {
     [...document.getElementsByClassName("p4_input")].forEach((input) => {
         if (p4Selector.value != '')
@@ -25,7 +25,7 @@ p4Selector.addEventListener("change", () => {
         {
             input.removeAttribute("required");
         }
-    })
+    });
 
     if (p4Selector.value != '')
     {
@@ -35,4 +35,4 @@ p4Selector.addEventListener("change", () => {
     {
         p3Selector.removeAttribute("required");
     }
-})
+});
